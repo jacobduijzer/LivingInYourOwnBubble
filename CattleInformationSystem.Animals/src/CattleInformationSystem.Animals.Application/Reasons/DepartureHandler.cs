e2017ddd7367c3c23719affdecf7507caa15569a
Jacob Duijzer <jacob@duijzer.com>
2024-03-05 14:46:22 +0100

    Starting point for departure handler implementation

--- CattleInformationSystem.Animals/src/CattleInformationSystem.Animals.Application/Reasons/DepartureHandler.cs
+++ CattleInformationSystem.Animals/src/CattleInformationSystem.Animals.Application/Reasons/DepartureHandler.cs
@@ -5,20 +5,16 @@ namespace CattleInformationSystem.Animals.Application.Reasons;
 
 public class DepartureHandler(
     IAnimalAcl animals,
-    IEnumerable<Farm> farms,
+    IReadOnlyCollection<Farm> farms,
     AnimalCategoryDeterminationService categoryDetermination)
     : IReasonHandler
 {
     private readonly IAnimalAcl _animals = animals;
-    private readonly IEnumerable<Farm> _farms = farms;
+    private readonly IReadOnlyCollection<Farm> _farms = farms;
     private readonly AnimalCategoryDeterminationService _categoryDetermination = categoryDetermination;
 
     public async Task Handle(IncomingAnimalEventCreated incomingAnimalEvent)
     {
-        var currentFarm = farms.First(farm => farm.UBN.Equals(incomingAnimalEvent.CurrentUbn));
-        var destinationFarm = farms.First(farm => farm.UBN.Equals(incomingAnimalEvent.TargetUbn));
-        var animal = await animals.ByLifeNumber(incomingAnimalEvent.LifeNumber);
-        
         throw new NotImplementedException();
     }
 }
