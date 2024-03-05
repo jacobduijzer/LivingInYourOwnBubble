5c8db0e87ac37ec774f3516c9fd09c0ccb362a39
Jacob Duijzer <jacob@duijzer.com>
2024-03-05 16:47:24 +0100

    removed scripts

--- backwards.sh
+++
@@ -1,3 +0,0 @@
-git checkout .
-git clean -fd
-git checkout $(git log --pretty=%H --parents -n 2 | tail -n 1)
--- forward.sh
+++
@@ -1,3 +0,0 @@
-git checkout .
-git clean -fd
-git checkout $(git log --reverse --pretty=%H --ancestry-path HEAD..main | head -n 1)
